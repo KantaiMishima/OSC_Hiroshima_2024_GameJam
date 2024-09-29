1f74cefd2349b56352cae93f75218e4b3733d5bd
KantaiMishima <mishimakantai@gmail.com>
2024-09-29 13:19:29 +0900

    feat: 無限ジャンプ対策

--- player.gd
+++ player.gd
@@ -1,7 +1,12 @@
 extends RigidBody3D
 
 @export var speed = 5
-@export var jump = 5
+@export var jumpPower = 5
+var initialJumpTimes = 1
+var remainingJumpTimes = initialJumpTimes
+
+func _ready():
+	remainingJumpTimes = initialJumpTimes
 
 func _physics_process(delta):
 	get_input()
@@ -11,7 +16,13 @@ func get_input():
 	linear_velocity.x = input_direction.x * speed
 	linear_velocity.z = input_direction.y * speed
 	var input_space = Input.is_action_pressed("space")
-	if (position.y < 0.1 && input_space):
-		linear_velocity.y = jump
-		pass
-	pass
+	if (remainingJumpTimes > 0 && input_space):
+		linear_velocity.y = jumpPower
+		remainingJumpTimes -= 1
+
+func initJumpTimes():
+	remainingJumpTimes = initialJumpTimes
+
+func onEntered1(body_rid:RID, body:Node, body_shape_index:int, local_shape_index:int):
+	if (body.is_in_group("Floor")):
+		initJumpTimes()
